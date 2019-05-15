be450444279db8571ee66f7f96e1b0908cc83d3f
kushaGG <artem.kushniruk29@gmail.com>
2019-05-15 12:50:23 +0300

    "#2"

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,2 +1,29 @@
 class PhotosController < ApplicationController
+  def index
+    #code
+  end
+
+  def new
+    #code
+  end
+
+  def create
+    #code
+  end
+
+  def show
+    #code
+  end
+
+  def edit
+    #code
+  end
+
+  def update
+    #code
+  end
+
+  def destroy
+    #code
+  end
 end
